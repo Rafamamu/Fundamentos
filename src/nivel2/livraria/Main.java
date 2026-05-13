0b6c1848c11b44e2aa30e42dad219c2fa73a1d39
Rafael <rafaroga46@gmail.com>
2026-05-12 21:49:12 -0300

    feat: after a month, I've made a little progress in the main class by starting to work on the menu.

--- src/nivel2/livraria/Main.java
+++ src/nivel2/livraria/Main.java
@@ -15,24 +15,64 @@ public class Main {
         System.out.println("||          LIVRARIA SYSTEM                       ||");
         System.out.println("||                                                ||");
         System.out.println("====================================================");
-        System.out.print(" LOGIN: ");
+        System.out.print("LOGIN: ");
         String login = scanner.nextLine();
         System.out.print("SENHA: ");
         String senha = scanner.nextLine();
 
-         if (adm.validacaoLoginSenha(login, senha)) {
 
-             System.out.println("=================================================");
-             System.out.println("||                                              ||");
-             System.out.println("||  SEJA BEM VINDO RAFAEL BIBLIOTECÁRIO         ||");
-             System.out.println("||                                              ||");
-             System.out.println("||==============================================||");
+        while (!adm.validacaoLoginSenha(login, senha)) {
+
+            System.out.println("Login ou senha incorretos, digite novamente: ");
+            System.out.println("=============================================");
+
+            System.out.print("LOGIN: ");
+            login = scanner.nextLine();
+            System.out.print("SENHA: ");
+            senha = scanner.nextLine();
+        }
 
 
-         }
+        boolean rodando = true;
+
+        while (rodando) {
+            System.out.println("=================================================");
+            System.out.println("||                                              ||");
+            System.out.println("||  SEJA BEM VINDO RAFAEL BIBLIOTECÁRIO         ||");
+            System.out.println("||                                              ||");
+            System.out.println("||==============================================||");
+            System.out.println("|| Escolha uma opção:                           ||");
+            System.out.println("||==============================================||");
+            System.out.println("|| 1-Cadastrar novo usuário                     ||" +
+                             "\n|| 2-Cadastrar livro                            ||" +
+                             "\n|| 3-Listar usuários                            ||" +
+                             "\n|| 4-Listar livros                              ||" +
+                             "\n|| 5-Sair...                                    ||");
+
+
+            System.out.print("|| Digite o número da função desejada:            ||");
+            int opcao = 0;
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+
+                case 1:
+                    System.out.println("Digite o nome do usuário: ");
+                    String nome = scanner.nextLine();
+                    Usuario2 usuario = new Usuario2(nome);
+                    adm.cadastrarUsuario(usuario);
+
+
+
+            }
+
+
+        }
 
 
         scanner.close();
+
 
     }
 
